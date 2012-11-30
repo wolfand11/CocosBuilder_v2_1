317b22e93fe56267375ad75b34ccb25951a71a28
GuoDong <smile_guodong@163.com>
2012-11-30 20:57:40 +0800

    add new widget GTab

---
+++ PlugIn Nodes/GTab/Source/GTab.h
@@ -0,0 +1,13 @@
+//
+//  GTab.h
+//  GTab
+//
+//  Created by GuoDong on 12-11-30.
+//
+//
+
+#import "cocos2d.h"
+
+@interface GTab : CCMenu
+
+@end
---
+++ PlugIn Nodes/GTab/Source/GTab.m
@@ -0,0 +1,13 @@
+//
+//  GTab.m
+//  GTab
+//
+//  Created by GuoDong on 12-11-30.
+//
+//
+
+#import "GTab.h"
+
+@implementation GTab
+
+@end
